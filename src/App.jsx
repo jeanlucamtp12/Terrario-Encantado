791cfe08b441c9b093d127995e02ba3c3077de49
jeanlucamtp12 <jeaanlucamartins@gmail.com>
2024-01-20 13:40:24 -0300

    loading revertido

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,32 @@
-import React, { useState, useEffect } from 'react';
+import { useEffect, useState } from 'react'
+import audioSrc from './assets/som/tema.mp3'
+
+import Floresta from './Floresta'
 import Loading from './Loading';
 
-// Importe as imagens do seu arquivo
-import * as imagens from  './assets/imagens';
+function App() {
 
-const App = () => {
   const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    const imagensArray = [
-      imagens.nuvem,
-      imagens.arvore1,
-      imagens.arvore2,
-      imagens.arvore3,
-      imagens.cogumelo,
-      imagens.mato,
-      imagens.aranha,
-      imagens.cachorro_dormindo,
-      imagens.cachorro,
-      imagens.dino_voador,
-      imagens.dino,
-      imagens.gato,
-      imagens.ovelha,
-      imagens.morcego,
-      imagens.passaro,
-      imagens.sapo,
-    ];
+    useEffect(() => {
+        setTimeout(() => setLoading(false), 4000);
+    }, [])
+    if (loading) {
+        return <Loading/>
+    }
 
-    const promises = imagensArray.map((img) => {
-      return new Promise((resolve) => {
-        const imagem = new Image();
-        imagem.src = img;
-        imagem.onload = resolve;
-      });
-    });
 
-    Promise.all(promises)
-      .then(() => {
-        setLoading(false);
-      })
-      .catch((error) => {
-        console.error('Erro ao carregar recursos:', error);
-        setLoading(false);
-      });
-  }, []); // Executa apenas uma vez durante a montagem do componente
+  document.addEventListener('click', musicPlay);
 
-  if (loading) {
-    return <Loading />;
+  function musicPlay() {
+    const audio = new Audio(audioSrc);
+    audio.loop = true;
+    audio.volume = 0.3;
+
+    audio.currentTime = 0;
+    audio.play();
+    document.removeEventListener('click', musicPlay);
   }
-
-  // O restante do seu código aqui após o carregamento
 
   return (
     <div>
